aed6268c1ab22b479da4b47fc8ce28eee9a836a0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-09 22:09:59 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+double altura;
+double pesoideal;
+string sexo;
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.Write(" Digite sua altura aqui: ");
+
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write(" Sexo [M]asculino | [F]eminino: ");
+
+sexo = Console.ReadLine()!.ToUpper().Trim();
+
+if (sexo != "M" && sexo != "F")
+
+{
+    Console.WriteLine("  Não foi possível concluir.");
+}
+
+else
+
+{
+    if (sexo == "M")
+
+    {
+        pesoideal = altura * 72.7 - 58.0;
+    }
+
+    else
+
+    {
+        pesoideal = altura * 62.1 - 44.7;
+    }
+
+    Console.WriteLine($"Seu peso ideal é {pesoideal:N1}kg.");
+}
